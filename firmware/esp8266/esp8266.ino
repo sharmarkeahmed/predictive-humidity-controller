54aa4b24eeef072577e8d0c9545b82e481bd41fd
Mya <127999742+and10012@users.noreply.github.com>
2026-03-24 16:05:17 -0500

    Change API delay to 60 seconds and simplify logic
    Updated API call delay to 60 seconds and removed state machine implementation for simplified flow.

--- firmware/esp8266/esp8266.ino
+++ firmware/esp8266/esp8266.ino
@@ -19,8 +19,8 @@ String jsonBuffer;
 // Delay for calling the API
 unsigned long lastApiTime = 0;
 unsigned long lastUartTime = 0;
-unsigned long apiDelay = 900000;   // 15 minutes
-// unsigned long apiDelay = 60000; // 60 Seconds
+// unsigned long apiDelay = 900000;   // 15 minutes
+unsigned long apiDelay = 60000; // 60 Seconds
 unsigned long UARTDelay = 10000; // 10 Seconds
 
 // number of forecast hours we want
@@ -33,18 +33,6 @@ bool forecastValid = false;
 // UART sequence counter
 uint8_t sequence = 0;
 
-// State Machine States
-enum SystemState {
-  INIT,
-  WIFI_CONNECT,
-  IDLE,
-  FETCH_API,
-  PROCESS_DATA,
-  ERROR
-};
-
-// Initialize the current state as initializing
-SystemState currentState = INIT;
 
 String getHTTPRequest(const char* serverName); // Get HTTP request from API serverName = resource URL
 bool parseForecast(String json); // Takes in the raw forecast returned from the API and turns it into forecast temperature and humidity arrays
@@ -75,72 +63,41 @@ void setup() {
 }
 
 void loop() {
+
   unsigned long currentTime = millis();
-  
-  // Case state for the ESP to avoid blocking loop statements, this allows for setting different delays for the API and UART
-  switch(currentState) {
 
-    case INIT:
-      Serial.println("Initializing...");
-      currentState = WIFI_CONNECT;
-      break;
+  // API Task
+  if ((currentTime - lastApiTime) > apiDelay) {
 
-    case WIFI_CONNECT:
-      if (WiFi.status() == WL_CONNECTED) {
-        Serial.println("WiFi Connected");
-        Serial.print("IP: ");
-        Serial.println(WiFi.localIP());
-        currentState = IDLE;
-      } else {
-        Serial.print("Could not connect to WIFI");
-        currentState = INIT;
-      }
-
-      break;
-
-    case IDLE:
-      if ((currentTime - lastApiTime) > apiDelay) {
-        currentState = FETCH_API;
-      }
-      break;
-
-    case FETCH_API: {
+    if (WiFi.status() == WL_CONNECTED) {
 
       String serverPath =
-        "https://api.open-meteo.com/v1/forecast?latitude=" + latitude +
-        "&longitude=" + longitude +
-        "&hourly=temperature_2m,relative_humidity_2m" +
-        "&current=temperature_2m,relative_humidity_2m" +
-        "&timezone=America%2FChicago" +
-        "&forecast_days=3" +
-        "&temperature_unit=" + temperature_unit;
+      "https://api.open-meteo.com/v1/forecast?latitude=" + latitude +
+      "&longitude=" + longitude +
+      "&hourly=temperature_2m,relative_humidity_2m" +
+      "&current=temperature_2m,relative_humidity_2m" +
+      "&timezone=America%2FChicago" +
+      "&forecast_days=3" +
+      "&temperature_unit=" + temperature_unit;
 
       jsonBuffer = getHTTPRequest(serverPath.c_str());
 
-      currentState = PROCESS_DATA;
-      break;
+      Serial.println("JSON received");
+
+      parseForecast(jsonBuffer);
+
+    } else {
+
+      Serial.println("WiFi lost, reconnecting...");
+      WiFi.begin(WIFI_SSID, WIFI_PASS);
+
     }
 
-    case PROCESS_DATA:
-      if (parseForecast(jsonBuffer)) {
-        forecastValid = true;
-        Serial.println("Forecast updated");
-        currentState = IDLE;
-      } else {
-        Serial.println("Parse failed");
-        currentState = ERROR;
-      }
-      lastApiTime = currentTime;
-      break;
-
-    case ERROR:
-      Serial.println("Error occurred, retrying...");
-      currentState = IDLE;
-      break;
+    lastApiTime = currentTime;
   }
- 
+
   // UART Task
-  if ((currentTime - lastUartTime) > UARTDelay && forecastValid) {
+  if ((currentTime - lastUartTime) > UARTDelay) {
     sendForecastUART();
     lastUartTime = currentTime;
   }
@@ -197,6 +154,8 @@ bool parseForecast(String json) {
     }
   }
 
+  Serial.print("Current Hour:" + currentTime);
+
 // Store the forecasts into forecastTemp and forecastRH for the number of FORECAST_HOURS
   for (int i = 0; i < FORECAST_HOURS; i++) {
     forecastTemp[i] = (double) temperature[startIndex + i];
@@ -225,38 +184,20 @@ uint8_t computeChecksum(uint8_t* data, int len) {
 
 // Send UART data to the STM32
 void sendForecastUART() {
-  sequence++;   // For Debugging to see what packet we are on (number wise)
-  uint8_t buffer[128];
-  int index = 0;
-  int lengthIndex = index++;      // reserve length
 
-  // Payload [START 0XAA][LEN (in BYTES)][DATA][CHECKSUM][END 0X55]
+  //[START 1 BYTE][PAYLOAD LENGTH 1 BYTE][TEMPERATURES 32 BYTES][HUMIDITY 8 BYTES][END 1 BYTE]
 
-  // Store temperatures as floats
-  for (int i = 0; i < FORECAST_HOURS; i++) {
-    memcpy(&buffer[index], &forecastTemp[i], sizeof(float));
-    index += sizeof(float);
-  }
-
-  // Store Relative humidity as integers
-  for (int i = 0; i < FORECAST_HOURS; i++) {
-    buffer[index++] = forecastRH[i];
-  }
-
-  // Store length of data
-  uint8_t payloadLength = index - 1;
-  buffer[lengthIndex] = payloadLength;
-
-  uint8_t checksum = computeChecksum(buffer, index);
-  buffer[index++] = checksum;
-
-  // Send the frame
+  Serial.println("Sending forecast to STM32...");
   Serial1.write(0xAA);
-  Serial1.write(buffer, index);
-  Serial1.write(0x55);
-
-  Serial.print("UART sent | Seq: ");
-  Serial.println(sequence);
+  // Start Byte - use an if else on STM side to check
+  // send temperatures (floats = 4 bytes each)
+  for(int i = 0; i < FORECAST_HOURS; i++){
+    Serial1.write((uint8_t*)&forecastTemp[i], sizeof(float));
+  } // send humidity (1 byte each) 
+  for(int i = 0; i < FORECAST_HOURS; i++){ 
+    Serial1.write(forecastRH[i]);
+    }
+    Serial1.write(0x55); // END BYTE }
 }
 
 /* 
